ece2249c3573c940f44b49cc2397bac29fcfd4c2
Saurav Agarwal <agr.saurav1@gmail.com>
2017-07-30 18:06:13 -0400

    Added control kinematics

--- simTests/kinematics/kobuki.py
+++ simTests/kinematics/kobuki.py
@@ -3,13 +3,16 @@
 ## The first three elements of the configuration are (x, y, z)
 ## The next three are zyx Euler angles
 ## The function getTransform is for getting the rotation and the translation of the current position of the robot
+## The function velControlKin is for converting velocity control inputs (linear along local x, and angular about local z) into state vector
+## The function wheelControlKin is for converting wheel velocity control inputs (angular wheel left and right) into state vector
 
 from klampt import *
 from klampt import vis
 from klampt.vis.glcommon import GLWidgetPlugin
 from klampt.math import so3
 import mathUtils
-class kobukiHolonomic(object):
+
+class kobuki(object):
     def __init__ (self, robot, vis=None):
         self.robot = robot
         self.vis = vis
@@ -20,6 +23,38 @@ class kobukiHolonomic(object):
             self.vis.add("Robot",[rotMat, pt])
             self.vis.setAttribute("Robot", "size", 32)
             self.vis.edit("Robot")
+
+    def velControlKin(self, vel, omega, deltaT):
+        q = self.getConfig()
+        if abs(omega) < self.eps:
+            q[0] = q[0] + vel * deltaT * math.cos(q[2])
+            q[1] = q[1] + vel * deltaT * math.sin(q[2])
+        else:
+            rad = vel/omega
+            q[0] = q[0] - rad * math.sin(q[2]) + rad * math.sin(q[2] + omega * deltaT)
+            q[1] = q[1] + rad * math.cos(q[2]) - rad * math.cos(q[2] + omega * deltaT)
+            q[2] = q[2] + omega * deltaT
+        self.setConfig(q)
+
+
+    def wheelControlKin(self, w_l, w_r, deltaT):
+        q = self.getConfig()
+        v_l = w_l * self.wheelDia/2.0
+        v_r = w_r * self.wheelDia/2.0
+        if abs(v_r - v_l) < self.eps:
+            q[0] =  q[0] + v_l * deltaT * math.cos(q[2])
+            q[1] =  q[1] + v_l * deltaT * math.sin(q[2])
+        else:            
+            ## Distance from ICC to centre of axle
+            rad = (v_l + v_r)/(2*(v_r - v_l))
+            angVel = (v_r - v_l)/self.lenAxle
+            icc = [q[0] - rad * math.sin(q[2]), q[1] + rad * math.cos(q[2])]
+            cosOmegaDeltaT = math.cos(angVel * deltaT)
+            sinOmegaDeltaT = math.sin(angVel * deltaT)
+            q[0] = (q[0] - icc[0]) * cosOmegaDeltaT - (q[1] - icc[1]) * sinOmegaDeltaT + icc[0]
+            q[1] = (q[0] - icc[0]) * sinOmegaDeltaT + (q[1] - icc[1]) * cosOmegaDeltaT + icc[1]
+            q[2] = q[2] + angVel * deltaT
+        self.setConfig(q)
 
     def getConfig(self):
         q = self.robot.getConfig()
